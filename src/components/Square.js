c0a1ef88d272f4cbf81e9e6a0bafa13117322ce7
vidudaya <vnbandara@gmail.com>
2020-08-28 02:13:57 -0400

    update to Square.js

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 export default class Square extends React.Component {
+  constructor(props) {
+    // In JavaScript classes, you need to always call super when defining the constructor of a subclass. 
+    // All React component classes that have a constructor should start with a super(props) call.
+    super(props);
+    this.state = { value: null };
+  }
+
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button
+        className="square"
+        onClick={() => {
+          this.setState({value: 'X'});
+        }}
+      >
+        {this.state.value}
+      </button>
+    );
   }
 }
