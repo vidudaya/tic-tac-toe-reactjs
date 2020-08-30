5ac363248033b989431daee4bb16cdeeee96d5b8
vidudaya <vnbandara@gmail.com>
2020-08-30 01:33:17 -0400

    update to Board.js and Square.js to handle a click and lifting the state of the game to Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,8 +2,30 @@ import React from "react";
 import Square from "../components/Square";
 
 export default class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    // The Board component now maintains which squares are filled.
+    // We need to create a way for the Square to update the Board’s state.
+    // Since state is considered to be private to a component that defines it,
+    // we cannot update the Board’s state directly from Square.
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,22 +1,15 @@
 import React from "react";
 
 export default class Square extends React.Component {
-  constructor(props) {
-    // In JavaScript classes, you need to always call super when defining the constructor of a subclass. 
-    // All React component classes that have a constructor should start with a super(props) call.
-    super(props);
-    this.state = { value: null };
-  }
-
   render() {
     return (
       <button
         className="square"
         onClick={() => {
-          this.setState({value: 'X'});
+          this.props.onClick();
         }}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
